d4972fa65b10d7ab76a34e293d0b428e3e354f4b
ganchikov <ganchikovm@gmail.com>
2018-06-26 14:57:08 +0300

    configs updated - unused stuff removed

--- src/app/common/models/app-config.model.ts
+++ src/app/common/models/app-config.model.ts
@@ -2,25 +2,13 @@ export interface IAppConfig {
     env: {
         name: string;
     };
-    appInsights: {
-        instrumentationKey: string;
-    };
     logging: {
         console: boolean;
-        appInsights: boolean;
-    };
-    aad: {
-        requireAuth: boolean;
-        tenant: string;
-        clientId: string;
-
     };
     enableHttps: boolean;
     apiServer: {
         url: string;
         httpsUrl: string;
-        metadata: string;
-        rules: string;
     };
     auth0: {
         clientID: string,
